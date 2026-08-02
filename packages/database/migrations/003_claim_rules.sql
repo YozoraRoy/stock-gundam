6a59d84c35142315fbbb17465b4cfd1dfb82fa0e
royyozora <royyozora@users.noreply.gitlab.com>
2026-08-02 22:00:37 +0800

    fix: use ADD (not ADD COLUMN) in migration 003 for T-SQL compatibility

--- packages/database/migrations/003_claim_rules.sql
+++ packages/database/migrations/003_claim_rules.sql
@@ -1,7 +1,7 @@
-ALTER TABLE shareholder_gifts ADD COLUMN gift_status NVARCHAR(50);
-ALTER TABLE shareholder_gifts ADD COLUMN claim_rule NVARCHAR(50);
-ALTER TABLE shareholder_gifts ADD COLUMN claim_rule_source NVARCHAR(50);
-ALTER TABLE shareholder_gifts ADD COLUMN mops_gift_text NVARCHAR(4000);
-ALTER TABLE shareholder_gifts ADD COLUMN mops_meeting_date NVARCHAR(50);
-ALTER TABLE shareholder_gifts ADD COLUMN mops_source_url NVARCHAR(1000);
-ALTER TABLE shareholder_gifts ADD COLUMN mops_updated_at NVARCHAR(50);
+ALTER TABLE shareholder_gifts ADD gift_status NVARCHAR(50);
+ALTER TABLE shareholder_gifts ADD claim_rule NVARCHAR(50);
+ALTER TABLE shareholder_gifts ADD claim_rule_source NVARCHAR(50);
+ALTER TABLE shareholder_gifts ADD mops_gift_text NVARCHAR(4000);
+ALTER TABLE shareholder_gifts ADD mops_meeting_date NVARCHAR(50);
+ALTER TABLE shareholder_gifts ADD mops_source_url NVARCHAR(1000);
+ALTER TABLE shareholder_gifts ADD mops_updated_at NVARCHAR(50);
